abe26f8971e82c503c1c82b5124b26e4f837aa35
Coris12 <corina.guzman.est@tecazuay.edu.ec>
2022-06-02 16:56:56 +0700

    cambios
    cambios

--- src/app/api/authController.service.ts
+++ src/app/api/authController.service.ts
@@ -370,6 +370,47 @@ export class AuthControllerService {
     }
 
     /**
+     * Muestra la lista de usuarios en el sistema
+     * 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public searchEUsingGET(observe?: 'body', reportProgress?: boolean): Observable<Array<Usuario>>;
+    public searchEUsingGET(observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<Array<Usuario>>>;
+    public searchEUsingGET(observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<Array<Usuario>>>;
+    public searchEUsingGET(observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+        let headers = this.defaultHeaders;
+
+        // authentication (JWT) required
+        if (this.configuration.apiKeys && this.configuration.apiKeys["Authorization"]) {
+            headers = headers.set('Authorization', this.configuration.apiKeys["Authorization"]);
+        }
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            '*/*'
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+        ];
+
+        return this.httpClient.request<Array<Usuario>>('get',`${this.basePath}/auth/empleados`,
+            {
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
      * search
      * 
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
