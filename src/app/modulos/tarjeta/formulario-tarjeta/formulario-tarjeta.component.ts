b2393a2dd11c9d6e6b6b75a97ce2534a6205722b
WillianPanza11 <57450978+WillianPanza11@users.noreply.github.com>
2022-07-21 12:51:29 -0500

    Revert "validaciones"
    This reverts commit 4db6a2049422dcdb102fd43a5b58c14869e58712.

--- src/app/modulos/tarjeta/formulario-tarjeta/formulario-tarjeta.component.ts
+++ src/app/modulos/tarjeta/formulario-tarjeta/formulario-tarjeta.component.ts
@@ -9,7 +9,7 @@ import { AuthControllerService } from 'src/app/api/authController.service';
 import { TokenService } from 'src/app/service/token.service';
 import { NuevoUsuario } from '../../user/models/nuevo-usuario';
 import { AuthService } from '../../user/service/auth.service';
-import { takeUntil } from 'rxjs/operators';
+import {takeUntil} from 'rxjs/operators';
 import { DatosTarjetaDto } from 'src/app/model/datosTarjetaDto';
 
 @Component({
@@ -17,34 +17,34 @@ import { DatosTarjetaDto } from 'src/app/model/datosTarjetaDto';
   templateUrl: './formulario-tarjeta.component.html',
   styleUrls: ['./formulario-tarjeta.component.css']
 })
-export class FormularioTarjetaComponent implements OnInit, OnDestroy {
+export class FormularioTarjetaComponent implements OnInit , OnDestroy{
 
   private unsuscribes$ = new Subject<void>();
   ObjDatorTarj: DatosTarjetaDto = {
-    canton: null,
-    celular: null,
-    direccion: null,
-    id: null,
-    idRecidencia: null,
-    nacionalidad: null,
-    nombres: null,
-    pais: null,
-    parroquia: null,
-    provincia: null,
-  }
+    canton:null,
+    celular:null,
+    direccion:null,
+    id:null,
+    idRecidencia:null,
+    nacionalidad:null,
+    nombres:null,
+    pais:null,
+    parroquia:null,
+    provincia:null,
+}
 
   elementType = NgxQrcodeElementTypes.URL;
   correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
   value: string;
   buscarPerIdent: string;
 
-  totalRecords: number;
-  loading: boolean;
-  familiares: any;
+  totalRecords:number;
+  loading :boolean;
+  familiares:any;
 
   errMsj: string;
 
-  display; boleean;
+  display;boleean;
 
   nuevoUsuario: NuevoUsuario;
   //! datos para el usuario
@@ -128,22 +128,13 @@ export class FormularioTarjetaComponent implements OnInit, OnDestroy {
     });
   }
 
-
-  keyPress(event: KeyboardEvent) {
-    const pattern = /[0-9]/;
-    const inputChar = String.fromCharCode(event.charCode);
-    if (!pattern.test(inputChar)) {
-      event.preventDefault();
-    }
-  }
-
-  buscarPersonaPorIdentificacion() {
+  buscarPersonaPorIdentificacion(){
     this.personaService.searchDateTarjetaUserUsingGET(this.buscarPerIdent).pipe(takeUntil(this.unsuscribes$)).subscribe((res) => {
-      if (res.object != null) {
+      if (res.object!=null){
         this.ObjDatorTarj = res.object;
-      } else {
+      }else{
         this.mensajeError("ERROR AL BUSCAR!");
-        this.ObjDatorTarj = { canton: null, celular: null, direccion: null, id: null, idRecidencia: null, nacionalidad: null, nombres: null, pais: null, provincia: null, parroquia: null }
+        this.ObjDatorTarj = {canton:null,celular:null,direccion:null,id:null,idRecidencia:null,nacionalidad:null,nombres:null,pais:null,provincia:null, parroquia:null}
       }
     }, error => {
       this.mensajeError("ERROR AL BUSCAR!!");
