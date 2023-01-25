86eb2ba3fba2a0627b4ce7e7993ed0451bffcfc3
Coris12 <corina.guzman.est@tecazuay.edu.ec>
2023-01-25 00:16:02 -0500

    CAMBIOS
    CAMBIOS

--- src/app/modulos/RegistroAdm/admision/admision.component.ts
+++ src/app/modulos/RegistroAdm/admision/admision.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthControllerService } from 'src/app/api/authController.service';
+import { ResidenciaDto } from 'src/app/model/residenciaDto';
 
 @Component({
   selector: 'app-admision',
@@ -8,14 +10,117 @@ import { Component, OnInit } from '@angular/core';
 export class AdmisionComponent implements OnInit {
 
   constructor(
-     
-  ) { }
+    private persnaService: AuthControllerService,
 
+  ) { }
+  cel: string
+  direccion: string
+  buscarcedula: string;
+  idpersona: any;
+  sexo: string
+  buscarnombre: string
+  establecimiento = "C.E.M. MEDIVALLE";
+
+  res:ResidenciaDto={
+    barrio: null,
+    canton: null,
+    nacionalidad: null,
+    pais: null,
+    parroquia: null,
+    provincia: null,
+    zona: null
+  }
+
+  selectedValue: string;
   ngOnInit(): void {
+    
   }
 
 
-buscar(){
 
-}
+  buscarPersona() {
+    this.persnaService.listaUsingGET().subscribe((res) => {
+      //console.log(this.buscarcedula, this.buscarnombre);
+      for (let datos of res) {
+
+        if (this.buscarcedula != "" && this.buscarcedula != undefined) {
+          //console.log(datos.identificacion, this.buscarcedula);
+          //this.buscarnombre = ""
+          if (datos.identificacion == this.buscarcedula) {
+            this.idpersona = datos.id
+            this.buscarcedula = datos.identificacion
+            this.buscarnombre = datos.nombres
+            this.direccion = datos.direccion
+            this.cel = datos.celular
+            this.sexo = datos.sexo
+            break;
+            //        this.numHoja= datos.dondeesa lo del numero de hoja?????????????????????????????????? eso todavia no le pongo ajajja le qu jaejajjaja chiiiiiiiiiiii bueno le haces como le hago yo
+          }
+        } else if (this.buscarnombre != "" && this.buscarnombre != undefined) {
+          //console.log(datos.nombres, this.buscarnombre);
+          //this.buscarcedula = ""
+          if (datos.nombres == this.buscarnombre) {
+            this.idpersona = datos.id
+            this.buscarcedula = datos.identificacion
+            this.buscarnombre = datos.nombres
+            this.direccion = datos.direccion
+            this.sexo = datos.sexo
+            this.cel = datos.celular
+          }
+        }
+
+      }
+      console.log(res);
+
+    })
+  }
+
+
+  
+  validacionAlfanumerica(event) {
+    const patron = /[a-zA-ZÑñ0-9 ,:-]/;
+    const permitidos = event.keyCode;
+    if (permitidos === 8) {
+      return true;
+    } else if (patron.test(event.key)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  validacionNumerosLetras(event) {
+    const patron = /[a-zA-ZÑñ0-9 ]/;
+    const permitidos = event.keyCode;
+    if (permitidos === 8) {
+      return true;
+    } else if (patron.test(event.key)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  validarSoloLetras(event) {
+    const patron = /[a-zA-Z ]/;
+    const permitidos = event.keyCode;
+    if (permitidos === 8) {
+      return true;
+    } else if (patron.test(event.key)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  validacionSoloNumeros(event) {
+    const patron = /[0-9]/;
+    const permitidos = event.keyCode;
+    if (permitidos === 8) {
+      return true;
+    } else if (patron.test(event.key)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
